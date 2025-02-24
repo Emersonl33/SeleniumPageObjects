f463fb505a1d89057dc680e15eb6da388a3fcd40
Emerson Gomes <80440788+Emersonl33@users.noreply.github.com>
2025-02-24 13:26:41 -0300

    Update MainTests.java

--- src/test/java/br/com/inmetrics/casetest/tests/MainTests.java
+++ src/test/java/br/com/inmetrics/casetest/tests/MainTests.java
@@ -16,16 +16,16 @@ public class MainTests{
     private WebDriver browser;
     @BeforeEach
     public void setup(){
-        //Abrir o navegador
+    
         WebDriverManager.chromedriver().setup();
         this.browser = new ChromeDriver();
         browser.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        //Abrir o site de teste
+        
         Home home = new Home(browser);
         home.abrirSite();
 
-        //Clicar no Botao MyAccount
+        
         home.linkMyAccount();
 
     }
@@ -33,7 +33,7 @@ public class MainTests{
     @DisplayName("Registrar")
     public void testRegister() {
 
-        //Registrar o usuário
+        o
         Register registro = new Register(browser);
         registro.registrarUsuario("ornot175@gmail.com", "SPFC1993@");
 
@@ -41,22 +41,22 @@ public class MainTests{
     @Test
     @DisplayName("Testar Login com credenciais válidas")
     public void testLogincredvalida() {
-        //Preencher o formulário
+        
         Login login = new Login(browser);
         login.preencherFormulario("ornot175@gmail.com", "SPFC1993@");
 
-        //Testar se o Login foi realizado
+       
         Assert.assertTrue(login.LoginOk());
     }
 
     @Test
     @DisplayName("Testar Login com credenciais inválidas")
     public void testLogincredinvalida() {
-        //Preencher o formulário
+        
         Login login = new Login(browser);
         login.preencherFormulario("ornot175@gmail.com", "1234567");
 
-        //Testar se o Login foi realizado
+       
         Assert.assertTrue(login.LoginFail());
     }
 
